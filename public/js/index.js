5d5b92c11d6df4a4c7e5361b92ae03edd34076e5
kev <kevHTW@gmx.com>
2022-06-24 14:22:25 +0200

    Add reset a audio track

--- public/js/index.js
+++ public/js/index.js
@@ -9,13 +9,20 @@ const PauseBtn2 = document.getElementById("Pause2");
 const StartGlobalBtn = document.getElementById("StartGlobal");
 const PauseGlobalBtn = document.getElementById("PauseGlobal");
 
-const CurrentTimeBtn = document.getElementById("CurrentTime1");
+const CurrentTimeBtn1 = document.getElementById("CurrentTime1");
+const CurrentTimeBtn2 = document.getElementById("CurrentTime2");
+
+const ResetBtn1 = document.getElementById("Reset1");
+const ResetBtn2 = document.getElementById("Reset2");
 
 const TileBtn = document.getElementById("Tile");
 
 
 let audio1 = new Audio();
 let audio2 = new Audio();
+
+//audioContext.createMediaElementSource(audio1);
+//audioContext.createMediaElementSource(audio2);
 
 audio1.src = "sounds/default_sound.mp3"
 audio2.src = "sounds/LassdieSonneindeinHerz.mp3"
@@ -46,8 +53,21 @@ function AddPauseMode(btn, audio){
     });
 }
 
+function AddResetMode(btn, audio){
+    btn.addEventListener("click", () => {
+        console.log("Reset");
+        audio.currentTime = 0;
+    });
+}
+
+setInterval(()=> {CurrentTimeBtn1.textContent = audio1.currentTime.toString()}, 500);
+setInterval(()=> {CurrentTimeBtn2.textContent = audio2.currentTime.toString()}, 500);
+
 AddPlayMode(StartBtn1, audio1);
 AddPlayMode(StartBtn2, audio2);
+
 AddPauseMode(PauseBtn1, audio1);
 AddPauseMode(PauseBtn2, audio2);
 
+AddResetMode(ResetBtn1, audio1)
+AddResetMode(ResetBtn2, audio2)
