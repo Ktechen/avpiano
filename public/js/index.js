bf9bfc1cec9008ce0f9b29a41a838ce3d4ee0901
kev <kevHTW@gmx.com>
2022-06-24 11:22:29 +0200

    Add NodeModuleTo gitignore and add start and pause btn

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,21 @@
+const audioContext = new AudioContext();
 
-alert("test");
+const StartBtn = document.getElementById("Start");
+const PauseBtn = document.getElementById("Pause");
+
+const CurrentTimeBtn = document.getElementById("CurrentTime");
+
+let audio1 = new Audio();
+audio1.src = "sounds/default_sound.mp3"
+
+StartBtn.addEventListener("click", () => {
+    console.log("Play");
+    audio1.play();
+});
+
+PauseBtn.addEventListener("click", () => {
+    console.log("Pause");
+    audio1.pause();
+    CurrentTimeBtn.textContent = "Time: " + audio1.currentTime;
+});
+
