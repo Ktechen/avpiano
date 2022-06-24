040e2648d2c7bbe037512a186394e42f4e849414
kev <kevHTW@gmx.com>
2022-06-24 13:41:21 +0200

    Add run 2 Audios and add Start and Pause all func

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,55 @@
 const audioContext = new AudioContext();
 
-const StartBtn = document.getElementById("Start");
-const PauseBtn = document.getElementById("Pause");
+const StartBtn = document.getElementById("Start1");
+const PauseBtn = document.getElementById("Pause1");
 
-const CurrentTimeBtn = document.getElementById("CurrentTime");
+const StartBtn2 = document.getElementById("Start2");
+const PauseBtn2 = document.getElementById("Pause2");
+
+const StartGlobalBtn = document.getElementById("StartGlobal");
+const PauseGlobalBtn = document.getElementById("PauseGlobal");
+
+const CurrentTimeBtn = document.getElementById("CurrentTime1");
+
+const TileBtn = document.getElementById("Tile");
+
 
 let audio1 = new Audio();
+let audio2 = new Audio();
+
 audio1.src = "sounds/default_sound.mp3"
+audio2.src = "sounds/LassdieSonneindeinHerz.mp3"
+
+StartGlobalBtn.addEventListener("click", () => {
+    console.log("Play all");
+    audio1.play();
+    audio2.play()
+});
+
+PauseGlobalBtn.addEventListener("click", () => {
+    console.log("Pause all");
+    audio1.pause();
+    audio2.pause();
+});
 
 StartBtn.addEventListener("click", () => {
     console.log("Play");
     audio1.play();
 });
 
+StartBtn2.addEventListener("click", () => {
+    console.log("Play");
+    audio2.play();
+});
+
 PauseBtn.addEventListener("click", () => {
     console.log("Pause");
     audio1.pause();
-    CurrentTimeBtn.textContent = "Time: " + audio1.currentTime;
 });
+
+PauseBtn2.addEventListener("click", () => {
+    console.log("Pause");
+    audio2.pause();
+});
+
 
