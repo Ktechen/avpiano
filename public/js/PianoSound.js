c8072620a2455f0bbd44c6a7eb93fe725c2f3c26
KatOTB <kurlei@web.de>
2022-07-23 14:29:46 +0200

    added proper volume control for the piano and the drums

--- public/js/PianoSound.js
+++ public/js/PianoSound.js
@@ -3,61 +3,35 @@ import { PianoDataSet } from "./PianoDataSet.js";
 
 let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 let oscList = [];
-let mainGainNode = null;
-let keyboard = document.querySelector(".keys");
-let volumeControl = document.getElementById("volume x mt1");
-let customWaveform = null;
-let sineTerms = null;
-let cosineTerms = null;
 let osc;
-
+let mainGainNode = null;
+let volumeControl = document.getElementById("volume-button");
+const octaveDropdown = document.getElementById("octave-dropdown");
+let sineTerms = null;
 let noteFreq = new PianoDataSet().createNoteTable();
 
-const octaveDropdown = document.getElementById("octave-dropdown");
-octaveDropdown.addEventListener("change", (e) => changeOctave(e.target.value));
 
 export function getPianoAudioContext() {
   return audioContext;
 }
 
-function setup() {
-  changeOctave(4);
-
-  mainGainNode = audioContext.createGain();
-  mainGainNode.connect(audioContext.destination);
-
-  sineTerms = new Float32Array([0, 0, 1, 0, 1]);
-  cosineTerms = new Float32Array(sineTerms.length);
-  customWaveform = audioContext.createPeriodicWave(cosineTerms, sineTerms);
-
-  for (let i = 0; i < 9; i++) {
-    oscList[i] = {};
-  }
-}
-
-function changeOctave(layer) {
-  noteFreq.forEach((keys, index) => {
-    let keyList = Object.entries(keys);
-    if (index === Number(layer)) {
-      keyList.forEach((key) => {
-        setupKey(key[0], layer, key[1], document.getElementById(key[0]));
-      });
-      console.log(keyList);
-    }
-  });
-}
-
+/**
+ * saves frequency to a certain note on the equivalent key on the UI and sets up corresponding eventlisteners
+ */
 function setupKey(note, octave, freq, key) {
   key.dataset["octave"] = octave;
   key.dataset["note"] = note;
   key.dataset["frequency"] = freq;
 
-  key.addEventListener("mousedown", notePressed, false);
-  key.addEventListener("mouseup", noteReleased, false);
-  key.addEventListener("mouseover", notePressed, false);
-  key.addEventListener("mouseleave", noteReleased, false);
+  key.addEventListener("mousedown", notePressed);
+  key.addEventListener("mouseup", noteReleased);
+  key.addEventListener("mouseover", notePressed);
+  key.addEventListener("mouseleave", noteReleased);
 }
 
+/**
+ * triggers oscillator with given frequency and produces the desired note-sound
+ */
 function playTone(freq) {
   osc = audioContext.createOscillator();
   osc.connect(mainGainNode);
@@ -66,18 +40,15 @@ function playTone(freq) {
 
   let type = "sine";
 
-  if (type == "custom") {
-    osc.setPeriodicWave(customWaveform);
-  } else {
-    osc.type = type;
-  }
-
   osc.frequency.value = freq;
   osc.start();
 
   return osc;
 }
 
+/**
+ * responsible for keeping the "states" of the buttons up to date and responsible for triggering "playTone" whenever the state allows it to
+ */
 function notePressed(event) {
   if (event.buttons & 1) {
     let dataset = event.target.dataset;
@@ -90,6 +61,9 @@ function notePressed(event) {
   }
 }
 
+/**
+ * another function to alter states of the keys and kills oscillator on demand
+ */
 function noteReleased(event) {
   let dataset = event.target.dataset;
 
@@ -98,6 +72,43 @@ function noteReleased(event) {
     oscList[octave][dataset["note"]].stop();
     delete oscList[octave][dataset["note"]];
     delete dataset["pressed"];
+  }
+}
+
+/**
+ * changes keys on the piano to the desired octave (layer)
+ */
+function changeOctave(layer) {
+  noteFreq.forEach((keys, index) => {
+    let keyList = Object.entries(keys);
+    if (index === Number(layer)) {
+      keyList.forEach((key) => {
+        setupKey(key[0], layer, key[1], document.getElementById(key[0]));
+      });
+    }
+  });
+}
+
+function changeVolume(event) {
+	mainGainNode.gain.value = volumeControl.value
+}
+
+/**
+ * sets up eventlisteners for utility-functions, gives piano a default octave (4) and defines the form the of the sinus-wave used to create sounds in the oscillators
+ */
+function setup() {
+  changeOctave(3);
+  octaveDropdown.addEventListener("change", (e) => changeOctave(e.target.value));
+  volumeControl.addEventListener("change", changeVolume, false);
+  mainGainNode = audioContext.createGain();
+  mainGainNode.connect(audioContext.destination);
+  mainGainNode.gain.value = volumeControl.value;
+
+  sineTerms = new Float32Array([0, 0, 1, 0, 1]);
+
+
+  for (let i = 0; i < 9; i++) {
+    oscList[i] = {};
   }
 }
 
